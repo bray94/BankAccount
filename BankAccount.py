ce2d90fefb67fc2941da0329276189a231b900d9
bray94 <braydenturner94@gmail.com>
2015-06-12 17:46:32 -0500

    Incremental adjustments. Added classes to handle reading and writing to a file to store values.

--- BankAccount.py
+++ BankAccount.py
@@ -1,8 +1,9 @@
 class BankAccount:
+	"""Creates a bank account with Name on Account and starting value. It will pull
+	in either data from a file, or from the user if data doesn't exist"""
 
-	def __init__(self, startingValue, nameOnAccount):
+	def __init__(self, startingValue):
 		self.balance = startingValue
-		self.nameOnAccount = nameOnAccount
 		self.categories = []
 
 	def categoryExistence(self, name):
@@ -13,8 +14,8 @@ class BankAccount:
 		 return False
 
 
-	def addCategory(self, newCategory):
-		category = Category(newCategory)
+	def addCategory(self, newCategory, amount):
+		category = Category(newCategory, amount)
 		self.categories.append(category)
 
 	def updateCategory(self, name, amount):
@@ -33,21 +34,21 @@ class BankAccount:
 			updateCategory(category,amount)
 
 		else:
-			addCategory(category)
-			updateCategory(amount)
+			addCategory(category, amount)
 
 
-	def checkBalance():
+	def checkBalance(self):
 		return self.balance
 
 
 
 
 class Category:
+"""Class to add catehories that transasctions will be stores under """
 
-	def __init__(self, name):
+	def __init__(self, name, amount):
 		self.name = name
-		self.amount = 0.00
+		self.amount = amount
 
 	def updateCategory(amount):
 		self.amount+= amount
@@ -58,4 +59,44 @@ class Category:
 	def getName():
 		return self.name
 
+class WriteToFile:
+
+	def writeBankAccountToFile(BankAccount):
+		f = open("data.txt","r+")
+		for line in f:
+			if (line.split())[0] == "Balance:":
+				f.write("Balance: " + BankAccount.checkBalance() + "\n")
+			else: pass
+		f.close()
+
+	def writeCategoryToFile(Category):
+		f = open("data.txt","r+")
+		for line in f:
+			if (line.split())[0] == Category.getName() + ":":
+				f.write(Category.getName() + ": " + Category.getAmount() + "\n")
+			else: pass
+		f.close()
+			
+class ReadFile:
+
+	def initialRead():
+		f = open("data.txt" , "r")
+		for line in f:
+			if (line.split())[0] == "Balance:":
+				account = BankAccount(float(((line.split())[1])[1:]))
+			else:
+				account.addCategory(((line.split())[0])[0:-1] float(((line.split())[1])[1:]))
+		f.close()
+		return account
+
+
+def main():
+	account = initialRead()
+	interface(account)
+
+
+
+if __name__ == '__main__':
+	main()
 	
+
