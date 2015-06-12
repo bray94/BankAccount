071b718da9d9aa56272a38c68f0345684210cd63
Brayden Turner <braydent@wolfram.com>
2015-06-12 18:38:45 -0500

    Changed file name

---
+++ Interface.py
@@ -0,0 +1,21 @@
+class Interface:
+
+	def interface(account):
+		on = True
+
+		while on:
+			answer = raw_input("What would you like to do?\nAdd Transaction\nCheck Balance\nQuit\n")
+
+			if (answer.split()[0]).lower() == "add":
+				amount = input("How much was the transaction?")
+				account.addTransaction(amount)
+			elif (answer.split()[0]).lower() == "check":
+				print (account.checkBalance() + "\n")
+			elif (answer.split()[0]).lower() == "quit":
+				on = False
+			else:
+				print ("Not a valid command. Try again.")
+
+		
+
+
