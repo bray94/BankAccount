6ebc5b71a82aa79e8d8a0c127c11edcca4523069
Brayden Turner <braydent@wolfram.com>
2015-06-17 08:44:54 -0500

    Added space

--- Interface.py
+++ Interface.py
@@ -6,10 +6,10 @@ def interface(account):
 		answer = raw_input("What would you like to do?\nAdd Transaction\nDeposit\nCheck Balance\nQuit\n\n")
 
 		if (answer.split()[0]).lower() == "add":
-			amount = input("How much was the transaction?")
+			amount = input("How much was the transaction? ")
 			account.addTransaction(amount)
 		elif (answer.split()[0]).lower() == "deposit":
-			amount = input("How much would you like to deposit?")
+			amount = input("How much would you like to deposit? ")
 			account.deposit(amount)
 		elif (answer.split()[0]).lower() == "check":
 			print (str(account.checkBalance()) + "\n")
