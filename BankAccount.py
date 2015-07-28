dc638e143884ee2e146760286cdd3429c6bdb62a
Brayden Turner <braydent@wolfram.com>
2015-07-28 10:57:44 -0500

    Combined into 1 file

--- BankAccount.py
+++ BankAccount.py
@@ -1,5 +1,5 @@
-from Interface import *
 import pickle
+import Graphics as graph
 
 class BankAccount:
 	"""Creates a bank account with Name on Account and starting value. It will pull
@@ -68,6 +68,70 @@ class Category:
 	def getName(self):
 		return self.name
 
+class App:
+
+	def __init__(self, window, account):
+		self.account = account
+		self.window= window	
+
+		self.amount = Label(self.window, fg="red", text = "$"+str(self.account.checkBalance()))
+		self.amount.place(x=225, y=20)
+
+		self.addButton = Button(self.window, text = "Add Transaction" , command = self.addTransactionInput)
+		self.addButton.place(x=25, y=20)
+
+		self.depositButton = Button(self.window, text = "Deposit" , command = self.depositInput)
+		self.depositButton.place(x=25, y=70)
+
+		self.balanceButton = Button(self.window, text = "Check Balance" , command = self.checkBalanceDialog)
+		self.balanceButton.place(x=25, y=120)
+
+		self.quitButton = Button(self.window, text = "Quit" , command = self.quitProgram)
+		self.quitButton.place(x=25, y=170)
+
+		self.entry = Entry(self.window)
+		self.entry.place(x=225, y=120)
+		self.entry.pack
+
+		self.entry2 = Entry(self.window)
+
+		self.submit = Button(self.window, text = "Submit", command=self.submit)
+
+		
+	def addTransactionInput(self):
+		amount = int(self.entry.get())
+		
+		self.entry2.place 
+		self.submit.place(x=225, y=170)
+
+		self.entry.delete(0, END)
+		self.entry.insert(0, "Thanks!")
+
+
+		
+
+		amount = self.submit
+
+		print amount
+		print "A transaction would be added"
+
+	def submit(self):
+		return self.entry2.get()
+
+	def	submitCategory(self):
+		pass
+
+
+	def depositInput(self):
+		pass
+
+	def checkBalanceDialog(self):
+		print "balance would be checked"
+
+	def quitProgram(self):
+		self.account.quit()
+		self.window.quit()
+
 
 
 def writeBankAccountToFile(account):
@@ -86,6 +150,15 @@ def initialRead():
 		amount = float(raw_input("What is the starting amount?"))
 		newAccount = BankAccount(amount)
 		return newAccount
+
+def interface(account):
+	window = Tk()
+	window.geometry('450x220')
+	window.title('Money Manager')
+	app = App(window, account)
+
+	window.mainloop()
+
 
 def main():
 	account = initialRead()
